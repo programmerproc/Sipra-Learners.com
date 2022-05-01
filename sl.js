ac7b333cb3ccec00d15470ab282d38314b61c75d
programmerproc <104715705+programmerproc@users.noreply.github.com>
2022-05-02 01:00:47 +0500

    Add files via upload

---
+++ sl.js
@@ -0,0 +1,17 @@
+<html>
+<body>
+<p>    C language was developed in early 1970 by Danny Ritchie at Bell Laboratories.C has become one of the most popular<br>
+programming languages today.It is highly structured programming language that is easy to understand and use.In the past,<br>
+it was mainly used for writing system program such as operating systems, compilers, assemblers, etc.Today it is used for<br>
+writing all types of application programs as well such as word-processing programs, spreadsheet programs, database <br>
+management systems, educational programs, games etc.</p>
+<p id="kc"><b><pre>      C++ was developed by Bjarne Stroustrup also at Bell Laboratories during 1983-1985. C++  is a superset of C,<br>
+ meaning that any valid C program is also a valid C ++ program. The purpose of developing C++ was to provide programming<br>
+ facilities to easily and quickly write more powerful programs.<br></p></b></pre>
+ <script>
+ value of  id="kc" is 6
+ </script>
+ </body>
+ </html>
+ 
+ 
